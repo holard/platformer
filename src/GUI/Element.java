3e0d6b56b5a8f75bac0d2024632047a0a95339fa
joohyun1 <joohyun1@andrew.cmu.edu>
2015-03-09 23:03:37 -0400

    Element abstract class

---
+++ src/GUI/Element.java
@@ -0,0 +1,64 @@
+package GUI;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+public abstract class Element {
+	public int x,y;
+	public Image image;
+	boolean visible;
+	public int width, height;
+	public Map myMap;
+	public Board myBoard;
+	
+	public void epquery(boolean[] points, Tile[] tiles, int x, int y, int scale) {
+		// 0: topl, 1: top, 2: topr, 3: l, 4: r, 5: bl, 6: b, 7: br
+		points[0] = myMap.checkWall(x, y);
+		points[1] = myMap.checkWall(x + scale / 2, y);
+		points[2] = myMap.checkWall(x + scale, y);
+		points[3] = myMap.checkWall(x, y + scale / 2);
+		points[4] = myMap.checkWall(x + scale, y + scale / 2);
+		points[5] = myMap.checkWall(x, y + scale);
+		points[6] = myMap.checkWall(x + scale / 2, y + scale);
+		points[7] = myMap.checkWall(x + scale, y + scale);
+		tiles[0] = myMap.getWallAt(x, y);
+		tiles[1] = myMap.getWallAt(x + scale / 2, y);
+		tiles[2] = myMap.getWallAt(x + scale, y);
+		tiles[3] = myMap.getWallAt(x, y + scale / 2);
+		tiles[4] = myMap.getWallAt(x + scale, y + scale / 2);
+		tiles[5] = myMap.getWallAt(x, y + scale);
+		tiles[6] = myMap.getWallAt(x + scale / 2, y + scale);
+		tiles[7] = myMap.getWallAt(x + scale, y + scale);
+	}
+	
+	public Image getImage() {
+		return image;
+	}
+	
+	public void setImage(String file) {
+		ImageIcon ii = new ImageIcon(this.getClass().getResource(file));
+		image = ii.getImage();
+	}
+	
+	public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+    
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+}
